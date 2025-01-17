08f712854d2ec1cee7509b1279e001360531afc5
lidor1997 <lidorili8@gmail.com>
2025-01-17 15:19:43 +0200

    vercel url updated

--- recipes-client/src/services/axios/axios.ts
+++ recipes-client/src/services/axios/axios.ts
@@ -3,7 +3,7 @@ import { default as Axios } from "axios";
 import { appStorage } from "../appStorage";
 
 export const axios = Axios.create({
-  baseURL: "https://dr-cook-api.up.railway.app/api",
+  baseURL: "https://dr-cook.vercel.app/",
 });
 
 axios.interceptors.request.use(async (config) => {
